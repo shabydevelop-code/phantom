7c3fa5df46252ce09953a343ad3c808aea0ffded
shabydevelop-code <shaby.develop@gmail.com>
2026-06-12 17:19:26 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,41 @@
+const CACHE_NAME = 'phantom-setup-v56';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'icon_192.png',
+  'icon_512.png',
+  'phantom_step.wav',
+  'phantom_game_over.wav',
+  'phantom_sprite.png'
+];
+
+self.addEventListener('install', (e) => {
+  self.skipWaiting();
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    }).then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((cachedResponse) => {
+      return cachedResponse || fetch(e.request);
+    })
+  );
+});
